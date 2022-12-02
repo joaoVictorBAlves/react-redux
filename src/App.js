06c46e5aa2918fdbe19b6cba99618e01cc5decc7
João Victor <joaovba2002@alu.ufc.br>
2022-12-02 01:08:23 -0300

    Adjusts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Counter from './features/counter/Counter';
 function App() {
   return (
     <div className="App">
+      <h3>Counter</h3><br/>
+      <h4>App React & Redux</h4>
       <Counter />
     </div>
   );
